d8b6c8d413988f8ec73f49d122c19703549f0600
Nicolo <nicolorizzi1996@gmail.com>
2020-04-27 16:03:12 +0200

    INIT JS STRUCTURE

--- js/main.js
+++ js/main.js
@@ -4,7 +4,27 @@ $(document).ready(function () {
 Griglia 6x6 (36 boxes), ad ogni click parte una richiesta AJAX che prende un numero random da 1 a 9 (scegliere API opportuna).
 Se è <= 5 il quadrato diventa giallo, se è > di 5 il quadrato diventa verde.
 Il numero ottenuto appare al centro del quadrato
-DevTools console e network sono nostri amici :wink:
+DevTools console e network sono nostri amici
    */
-  
-});
+
+  //ref
+  var myApi = "https://flynn.boolean.careers/exercises/api/random/int";
+  var boxes = $(".cell");
+  boxes.click(function () {
+    var self = $(this);
+    console.log(self);
+    
+    $.ajax({
+      type: "GET",
+      url: myApi,
+      success: function (data) {
+        $('#random-number').text(data.response);
+      },
+      error: function (data) {
+        alert('ERROR', 'Si è verificato un errore');
+
+      }
+    });
+  });
+
+}); // END DOC READY
